03409c6e46aa0569414338e2f0abbbecdf85cd0a
Windell Grace Cabarles <wgcabarles2024@hccci.edu.ph>
2026-09-13 20:17:07 +0800

    Task 3: add basic route table for all four pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { Routes, Route } from "react-router";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
+import CountriesPage from "./pages/CountriesPage";
+import BucketListPage from "./pages/BucketListPage";
+import AboutPage from "./pages/AboutPage";
+
 
 const App = () => {
   // TODO: replace this with a real route table using react-router.
   // For now the app only shows HomePage. Clicking nav links causes a full
   // page reload (or a 404) because there's no client-side routing yet.
   return (
-    <Layout>
-      <HomePage />
-    </Layout>
+     <Routes>
+      <Route path="/" element={<Layout><HomePage /></Layout>} />
+      <Route path="/countries" element={<Layout><CountriesPage /></Layout>} />
+      <Route path="/bucket-list" element={<Layout><BucketListPage /></Layout>} />
+      <Route path="/about" element={<Layout><AboutPage /></Layout>} />
+    </Routes>
   );
 };
 
