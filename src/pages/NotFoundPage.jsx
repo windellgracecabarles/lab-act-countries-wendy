2639ad2be06d65f219ceb337a230b0f0045b43fb
Windell Grace Cabarles <wgcabarles2024@hccci.edu.ph>
2026-09-13 21:39:43 +0800

    Task 7: fix class and a-href bugs in NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,42 +1,47 @@
+import { Link } from "react-router";
+
 const NotFoundPage = () => {
   return (
-    <div class="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div class="text-center">
-        <div class="mx-auto flex h-16 w-16 items-center justify-center rounded-full bg-indigo-50">
+    <div className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full bg-indigo-50">
           <svg
-            class="h-10 w-10 text-indigo-600"
+            className="h-10 w-10 text-indigo-600"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
             />
           </svg>
         </div>
 
-        <p class="mt-6 text-base font-semibold text-indigo-600">404</p>
-        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <p className="mt-6 text-base font-semibold text-indigo-600">404</p>
+
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Page not found
         </h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">
+
+        <p className="mt-6 text-base leading-7 text-gray-600">
           Sorry, we couldn't find the page you're looking for. It might have
           been moved or deleted.
         </p>
 
-        <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
-            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
+          </Link>
+
           <a
             href="#"
-            class="text-sm font-semibold text-gray-900 hover:text-indigo-600"
+            className="text-sm font-semibold text-gray-900 hover:text-indigo-600"
           >
             Contact support <span aria-hidden="true">&rarr;</span>
           </a>
