f811dd48edebf68f15fd8a3cc6685c02efc21820
Windell Grace Cabarles <wgcabarles2024@hccci.edu.ph>
2026-09-13 20:54:22 +0800

    Task 6: add 404 catch-all route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 const App = () => {
@@ -12,12 +13,15 @@ const App = () => {
   // page reload (or a 404) because there's no client-side routing yet.
   return (
      <Routes>
-      <Route path="/" element={<Layout><HomePage /></Layout>} />
-      <Route path="/countries" element={<Layout><CountriesPage /></Layout>} />
-      <Route path="/bucket-list" element={<Layout><BucketListPage /></Layout>} />
-      <Route path="/about" element={<Layout><AboutPage /></Layout>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="countries" element={<CountriesPage />} />
+        <Route path="bucket-list" element={<BucketListPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 };
-
 export default App;
+
